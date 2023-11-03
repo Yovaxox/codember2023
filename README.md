e5fffd0346e45f7df580f820d2fce124ac292e4b
javier.paredes@outlook.com <javier.paredes@outlook.com>
2023-11-02 22:15:42 -0300

    first commit to the codember2023 repository challenges

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# codember2023
+# My solutions in codember's challenges 2023.
+
 This repository contains the solutions I have created for the codember's challenges in 2023.
+
+For this occasion, I used JavaScript in order to find the solution.
+
+The used the extension "Code Runner" to compile my JavaScript code.
+
+# Solution 1
+
+The following sentence applies for the first challenge:
+
+murcielago15leon15jirafa15cebra6elefante15rinoceronte15hipopotamo15ardilla15mapache15zorro15lobo15oso15puma2jaguar14tigre10leopardo10gato12perro12caballo14vaca14toro14cerdo14oveja14cabra14gallina10pato10ganso10pavo10paloma10halcon11aguila11buho11colibri9canario8loro8tucan8pinguino7flamenco7
+
+# Solution 2
+
+In process.
---
+++ challenge_01.js
@@ -0,0 +1,27 @@
+const data =
+  "murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco tigre jaguar leopardo oso lobo zorro mapache ardilla elefante rinoceronte hipopotamo cebra jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago flamenco pinguino tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon";
+
+const objectData = data
+   // It converts the variable "data" into an array by identifying blank spaces.
+  .split(" ")
+  // Reduce method applied on data variable once it was converted into an array. Its utility is to iterate an array and to counter elements.
+  .reduce( 
+    // Accumulator parameter which its objective is to accumulate during the iteration. Current parameter indicates the current element of the index.
+    (accumulator, current) => (
+    // accumulator[current] = accumulator[current] + 1 access to the actual value in the "accumulator" object. If it doesn't exist, it will initialize with the default value 1.
+    // accumulator[current] + 1 increments the current count by 1.
+    // accumulator[current] + 1 || 1 if the element wasn't in the object "accumulator", it will initialize the count in the value 1.
+    (accumulator[current] = accumulator[current] + 1 || 1), accumulator
+    ),
+    // It indicates the result variable is object data type.
+    {}
+  );
+
+let dataString = "";
+// This iteration access to the keys and values of the variable "objectData".
+// After that, it concatenates and save the result into a variable the key and value in 1 string.
+for (const [key, value] of Object.entries(objectData)) {
+    dataString = dataString.concat(`${key}${value}`);
+}
+
+console.log(dataString);
